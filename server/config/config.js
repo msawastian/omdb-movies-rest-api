fee9722b200483b2f3b4114d6eba6c67090d91d7
m.sawastian <m.sawastian@gmail.com>
2018-08-13 21:21:37 +0200

    Config cleanup.

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,8 @@
 let env = process.env.NODE_ENV || 'development';
 
-if (env === 'development' || env === 'test') {
-    const config = require('./config.json');
-    const envConfig = config[env];
-
-    Object.keys(envConfig).forEach(key => {
-        process.env[key] = envConfig[key];
-    })
+if (env === 'development') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = "mongodb://localhost:27017/omdb-movies";
 }
 
 console.log(`Running in ${env} mode`);
