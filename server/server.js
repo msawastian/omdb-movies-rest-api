3b2a6690ec7d244ab7049bf8a0b1f4c32dac97f6
m.sawastian <m.sawastian@gmail.com>
2018-08-21 15:26:55 +0200

    All of the routes use async/await now.

--- server/server.js
+++ server/server.js
@@ -14,33 +14,31 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json()); //required to parse request.body
 
-app.post('/movies', (request, response) => {
+app.post('/movies', async (request, response) => {
 
     if (!request.body.name) {
         return response.status(400).send({error: "Movie name required!"})
     }
 
-    axios.get(`https://www.omdbapi.com/?t=${encodeURIComponent(request.body.name)}&apikey=${process.env.OMDB_API_KEY}`)
-        .then(response => {
-            if (response.data.Response === 'False') {
-                throw new Error('Movie not found!')
-            }
+    try {
+        const movieResponse = await axios.get(`https://www.omdbapi.com/?t=${encodeURIComponent(request.body.name)}&apikey=${process.env.OMDB_API_KEY}`);
 
-            return response.data
-        })
-        .then(movieData => {
-            let movie = new Movie({
-                name: request.body.name,
-                movieData
-            });
+        if (movieResponse.data.Response === 'False') {
+            throw new Error('Movie not found!')
+        }
 
-            return movie.save()
-        })
-        .then(document => response.send(document))
-        .catch(error => response.status(400).send({error: error.message}));
+        const movie = await new Movie({
+            name: request.body.name,
+            movieData: movieResponse.data
+        }).save();
+
+        response.send(movie)
+    } catch (error) {
+        response.status(400).send({error: error.message})
+    }
 });
 
-app.get('/movies', (request, response, next) => {
+app.get('/movies', async (request, response, next) => {
 
     const action = request.header('action'),
         actions = ['sort', 'filter'];
@@ -72,31 +70,44 @@ app.get('/movies', (request, response, next) => {
 
     switch (action) {
         case 'sort':
-            Movie.find({})
-                .sort({
+            try {
+                const movies = await Movie.find({}).sort({
                     [criterion]: direction
-                })
-                .then(movies => response.send(movies))
-                .catch(error => response.status(400).send({error: error.message}));
+                });
+                response.send(movies)
+
+            } catch (error) {
+                response.status(400).send({error: error.message})
+            }
             break;
 
         case 'filter':
-            Movie.find({[criterion]: query})
-                .then(movies => {
-                    if (!movies.length) {
-                        throw new Error('No movies matching given query were found in the database!')
-                    }
-                    response.send(movies)
-                })
-                .catch(error => response.status(404).send({error: error.message}));
+            try {
+                const movies = await Movie.find({[criterion]: query});
+
+                if (!movies.length) {
+                    throw new Error('No movies matching given query were found in the database!')
+                }
+
+                response.send(movies)
+
+            } catch (error) {
+                response.status(404).send({error: error.message})
+            }
     }
 });
 
-app.get('/movies', (request, response) => {
-    Movie.find({}).then(movies => response.send(movies))
+app.get('/movies', async (request, response) => {
+    try {
+        const movies = await Movie.find({});
+
+        response.send(movies);
+    } catch (error) {
+        response.status(400).send({error: error.message})
+    }
 });
 
-app.post('/comments', (request, response) => {
+app.post('/comments', async (request, response) => {
     const movieID = request.body.id,
         text = request.body.text;
 
@@ -106,25 +117,27 @@ app.post('/comments', (request, response) => {
         });
     }
 
-    Movie.findById(movieID)
-        .then(document => {
-            if (!document) {
-                throw new Error('No movie with such ID in database!')
-            } else if (!request.body.text) {
-                throw new Error('Comment text required!')
-            }
+    try {
+        const movie = await Movie.findById(movieID);
 
-            return new Comment({
-                movieID,
-                text
-            });
-        })
-        .then(comment => comment.save())
-        .then(document => response.send(document))
-        .catch(error => response.status(400).send({error: error.message}))
+        if (!movie) throw new Error('No movie with such ID in database!');
+        if (!request.body.text) throw new Error('Comment text required!');
+
+        const comment = new Comment({
+            movieID,
+            text
+        });
+
+        const savedComment = await comment.save();
+
+        response.send(savedComment)
+
+    } catch (error) {
+        response.status(400).send({error: error.message})
+    }
 });
 
-app.get('/comments/:id?', (request, response, next) => {
+app.get('/comments/:id?', async (request, response, next) => {
     const movieID = request.params.id;
 
     if (!movieID) { //fallback for cases where ID wasn't provided.
@@ -138,25 +151,26 @@ app.get('/comments/:id?', (request, response, next) => {
         });
     }
 
-    Comment.find({movieID: movieID})
-        .then(document => {
-            if (!document.length) {
-                return response.status(404).send({error: 'No movie with such ID in database or no comments for movie with this ID!'})
-            }
-
-            response.send(document)
-        })
-        .catch(error => response.status(400).send({error: error.message}))
+    try {
+        const comment = await Comment.find({movieID: movieID});
+        if (!comment.length) {
+            return response.status(404).send({error: 'No movie with such ID in database or no comments for movie with this ID!'})
+        }
+        response.send(comment)
+    } catch (error) {
+        response.status(400).send({error: error.message})
+    }
 });
 
-app.get('/comments', (request, response) => {
-    Comment.find({})
-        .then(document => response.send(document))
-        .catch(error => response.status(400).send({error: error.message}))
+app.get('/comments', async (request, response) => {
+    try {
+        const comments = await Comment.find({});
+        response.send(comments)
+    } catch (error) {
+        response.status(400).send({error: error.message})
+    }
 });
 
-app.listen(port, () => {
-    console.log(`Server up and running on port ${port}`);
-});
+app.listen(port, () => {});
 
 module.exports = {app}; //for use in testing
